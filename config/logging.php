2c323a9865efacdfade3ebf77fef07cdc2ebb392
nadenggansrgr <talithaansiregar@gmail.com>
2025-04-23 16:04:56 +0700

    Disable AWS and Slack configurations

--- config/logging.php
+++ config/logging.php
@@ -73,14 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => env('LOG_SLACK_USERNAME', 'Laravel Log'),
-            'emoji' => env('LOG_SLACK_EMOJI', ':boom:'),
-            'level' => env('LOG_LEVEL', 'critical'),
-            'replace_placeholders' => true,
-        ],
+        // 'slack' => [
+        //     'driver' => 'slack',
+        //     'url' => env('LOG_SLACK_WEBHOOK_URL'),
+        //     'username' => env('LOG_SLACK_USERNAME', 'Laravel Log'),
+        //     'emoji' => env('LOG_SLACK_EMOJI', ':boom:'),
+        //     'level' => env('LOG_LEVEL', 'critical'),
+        //     'replace_placeholders' => true,
+        // ],
 
         'papertrail' => [
             'driver' => 'monolog',
